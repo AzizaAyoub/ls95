9ca357977ccd6a4211c56d50831c194af21846a8
AzizaAyoub <meaziza0@gmail.com>
2023-07-15 15:25:40 +0430

    exercise11 is done

---
+++ the_cli/exercise11.sh
@@ -0,0 +1,19 @@
+# Exit out of the root user session:
+
+root@host:/home/ubuntu# exit
+$
+
+# Log in as root in root's home directory:
+
+$ sudo su -
+Password:
+root@host:~# exit
+$
+
+# Run a command as root:
+
+$ ls /root
+ls: cannot open directory /root: Permission denied
+$ sudo ls /root
+Password:
+$
